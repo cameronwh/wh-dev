26400bedc79f4bf11dec51826780bb5c9b7276d3
Cameron Walker-Hebborn <cam@walker-hebborn.dev>
2022-10-04 00:52:08 +0100

    added firebase to app

--- wh-dev/src/app/app.module.ts
+++ wh-dev/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app'
 
 import { AppComponent } from './app.component';
+import { environment } from './../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase))
   ],
   providers: [],
   bootstrap: [AppComponent]
